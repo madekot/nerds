5257a6fe52a50766f5dcfa21b41d3a27da610142
madekot <mgolubcov@gmail.com>
2018-10-27 16:18:40 +0300

    Реализовал сохранения name и email в localStoradge

--- source/js/sсript.js
+++ source/js/sсript.js
@@ -1,18 +1,42 @@
 'use strict'
 
 var modalPopup = document.querySelector('.modal');
-var namePopup = modalPopup.querySelector('#name');
 var openPopup = document.querySelector('.map__button');
 var closePopup = modalPopup.querySelector('.modal__button-close');
 
+var formPopup = modalPopup.querySelector('.modal-form');
+var formSubmitPopup = formPopup.querySelector('[type = submit]');
+var namePopup = formPopup.querySelector('#name');
+var emailPopup = formPopup.querySelector('#email');
+var comentPopup = formPopup.querySelector('#text-modal');
+
+var storageName = localStorage.getItem('name');
+var storageEmail = localStorage.getItem('email');
 
 openPopup.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalPopup.classList.remove('visually-hidden');
-  namePopup.focus();
+  if (storageName && storageEmail) {
+    namePopup.value = storageName;
+    emailPopup.value = storageEmail;
+    comentPopup.focus();
+  } else {
+    namePopup.focus();
+  }
 });
 
 closePopup.addEventListener('click', function(evt) {
   evt.preventDefault();
   modalPopup.classList.add('visually-hidden');
 });
+
+formPopup.addEventListener('submit', function(evt) {
+  if (!namePopup.value || !emailPopup.value) {
+    evt.preventDefault();
+    console.log('Нужно ввести пароль');
+  } else {
+    evt.preventDefault();
+    localStorage.setItem('name', namePopup.value);
+    localStorage.setItem('email', emailPopup.value);
+  }
+})
