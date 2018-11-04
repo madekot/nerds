e452c6869aca1de5630d608cdae9b6c27a9178c0
madekot <mgolubcov@gmail.com>
2018-11-04 18:41:37 +0300

    Оптимизировал код

--- source/js/sсript.js
+++ source/js/sсript.js
@@ -12,34 +12,7 @@ var namePopup = formPopup.querySelector('#modalForm-name');
 var emailPopup = formPopup.querySelector('#email');
 var comentPopup = formPopup.querySelector('#text-modal');
 var popupButtonSubmit = formPopup.querySelector('.modal-form__button');
-
-
-var toCheckIsStorageSupport = function () {
-  var isStorageSupport = true;
-  var storage = '';
-  try {
-    storage = localStorage.getItem('name');
-    storage = localStorage.getItem('email');
-  } catch (err) {
-    isStorageSupport = false;
-  };
-  return isStorageSupport;
-};
-
-var readLocalStorage = function() {
-  if (toCheckIsStorageSupport()) {
-    var storageName = localStorage.getItem('name');
-    var storageEmail = localStorage.getItem('email');
-
-    if (storageName !== null && storageEmail !== null) {
-      namePopup.value = storageName;
-      emailPopup.value = storageEmail;
-      comentPopup.focus();
-    } else {
-      namePopup.focus();
-    }
-  }
-}
+var testElement = document.querySelector('main');
 
 var onNamePopupEmailPopupSubmit = function(evt) {
   if (toCheckIsStorageSupport()) {
@@ -80,6 +53,33 @@ var onRequaredFildsFormKeyup = function(evt) {
   }
 };
 
+var toCheckIsStorageSupport = function () {
+  var isStorageSupport = true;
+  var storage = '';
+  try {
+    storage = localStorage.getItem('name');
+    storage = localStorage.getItem('email');
+  } catch (err) {
+    isStorageSupport = false;
+  };
+  return isStorageSupport;
+};
+
+var readLocalStorage = function() {
+  if (toCheckIsStorageSupport()) {
+    var storageName = localStorage.getItem('name');
+    var storageEmail = localStorage.getItem('email');
+
+    if (storageName !== null && storageEmail !== null) {
+      namePopup.value = storageName;
+      emailPopup.value = storageEmail;
+      comentPopup.focus();
+    } else {
+      namePopup.focus();
+    }
+  }
+}
+
 var showPopapAnimation = function(className) {
   modalPopup.classList.add(className);
   function deleteClassAnimationShow() {
@@ -98,29 +98,62 @@ var closePopap = function(evt) {
   evt.preventDefault();
   modalPopup.classList.add('visually-hidden');
   document.removeEventListener('keydown', onModalPopupKeydown);
-  formPopup.removeEventListener('invalid', onRequaredFildsFormInvalid, true);
 }
 
-popupButtonOpen.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  openPopap(evt);
-  console.log(modalPopup.classList.contains('visually-hidden'))
+var toCheckIsStorageSupport = function () {
+  var isStorageSupport = true;
+  var storage = '';
+  try {
+    storage = localStorage.getItem('name');
+    storage = localStorage.getItem('email');
+  } catch (err) {
+    isStorageSupport = false;
+  };
+  return isStorageSupport;
+};
+
+var readLocalStorage = function() {
+  if (toCheckIsStorageSupport()) {
+    var storageName = localStorage.getItem('name');
+    var storageEmail = localStorage.getItem('email');
+
+    if (storageName !== null && storageEmail !== null) {
+      namePopup.value = storageName;
+      emailPopup.value = storageEmail;
+      comentPopup.focus();
+    } else {
+      namePopup.focus();
+    }
+  }
+}
+
+var addEventValidationPopup = function() {
   if (modalPopup.classList.contains('visually-hidden') === false) {
-    readLocalStorage();
     formPopup.addEventListener('invalid', onRequaredFildsFormInvalid, true);
     formPopup.addEventListener('submit', onNamePopupEmailPopupSubmit);
     formPopup.addEventListener('keyup', onRequaredFildsFormKeyup);
     popupButtonSubmit.addEventListener('click', onRequaredFildsFormClick);
   }
-});
+}
 
-popupButtonClose.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  closePopap(evt);
+var removeEventValidationPopup = function() {
   if (modalPopup.classList.contains('visually-hidden')) {
     formPopup.removeEventListener('invalid', onRequaredFildsFormInvalid, true);
     formPopup.removeEventListener('submit', onNamePopupEmailPopupSubmit);
     formPopup.removeEventListener('keyup', onRequaredFildsFormKeyup);
     popupButtonSubmit.removeEventListener('click', onRequaredFildsFormClick);
   }
+}
+
+popupButtonOpen.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  openPopap(evt);
+  readLocalStorage();
+  addEventValidationPopup();
+});
+
+popupButtonClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  closePopap(evt);
+  removeEventValidationPopup();
 });
